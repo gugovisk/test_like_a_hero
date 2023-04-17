37dd27b55c038d0859189f1d20cbf49a7c66be68
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-04-17 14:29:12 -0300

    feat:[Challenge] Testa projeto (model weapon) usando a gem FFaker

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
   it "is valid if the level is between 1 and 99" do
-    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:12 )).to be_valid
+    weapon = build(:weapon, level: FFaker::Random.rand(1..99))
+    expect(weapon).to be_valid
   end
   it "is invalid if current_power is negative" do
-    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: -200, power_step: 150, level:12 )).to_not be_valid
+    weapon = build(:weapon, power_base: FFaker::Random.rand(-9999..0))
+    expect(weapon).to_not be_valid
   end
   it "it returns the correct weapon title" do
-    weapon = Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:12 )
-    expect(weapon.title).to eq ("Faquinha #12")
+    name = FFaker::Name.first_name
+    level = FFaker::Random.rand(1..99)
+    weapon = build(:weapon, name: name, level: level)
+    expect(weapon.title).to eq ("#{name} ##{level}")
   end
 end
