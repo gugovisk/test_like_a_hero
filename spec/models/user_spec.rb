aee471b78aa7d90ba659663a9034922b4943dc18
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-04-17 13:28:45 -0300

    Testa projeto (model user) com a gem FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
-
-    expect(user).to_not be_valid
+    user = build(:user, level: FFaker::Random.rand(100..9999))
+    expect(user).to_not be_valid # não vai ser válido, porque vai ter um level que NÃO está entre 1 e 99
   end
   it "returns the correct hero title" do
     nickname = FFaker::Name.first_name
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
 
-    user = User.create(nickname: nickname, kind: kind, level: level)
+    user = create(:user, nickname: nickname, kind: kind, level: level)
     expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
-
 end
